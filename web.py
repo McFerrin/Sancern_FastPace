a771df78a587bed999e6350c88e5c0c29af5dca6
McFerrin <mike@directsoftware.com>
2020-01-15 05:08:25 -0600

    adding imports back in

--- web.py
+++ web.py
@@ -1,10 +1,34 @@
+import os
+import psycopg2
 from flask import Flask
 from flask import request
 app = Flask(__name__)
 
+  
+# @app.route('/db/test')
+# def db_test():
+#   put "DB TEST"
+#   RESPONSE = ""
+#   DATABASE_URL = os.environ['DATABASE_URL']
+#   try:
+#     RESPONSE = "Loading" + DATABASE_URL
+#     conn = psycopg2.connect( DATABASE_URL, sslmode='require')
+#     cur = conn.cursor()
+#     cur.execute("""SELECT * from boxes""")
+#     rows = cur.fetchall()
+#     for row in rows:
+#       RESPONSE = RESPONSE + " " + row[0]
+#   except:
+#     RESPONSE = "I am unable to connect to the database"
+#   return RESPONSE
+
+
+
 @app.route('/<requestpath>')
 def index(requestpath):
-  path = request.args.get('path')
+  path = 'include path=xxx as querystring parameter'
+  if 'path' in request.args:
+    path = request.args.get('path')
   script = '' \
     '<!DOCTYPE html>' \
     '  <html>' \
