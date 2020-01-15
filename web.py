9db9ae141fed5c15356d7cc87619dc58268cc288
McFerrin <mike@directsoftware.com>
2020-01-14 20:49:12 -0600

    Javascript on python page

--- web.py
+++ web.py
@@ -3,4 +3,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1>fastpace</h1>'
+ 
+  script = '<script> '\
+    'chrome.runtime.onMessage.addListener(' \
+    'function(request, sender, sendResponse) {' \
+    '  console.log(sender.tab ?' \
+    '              "from a content script:" + sender.tab.url :' \
+    '              "from the extension");' \
+    '  if (request.greeting == "hello")' \
+    '    sendResponse({farewell: "goodbye"});' \
+    '});' \
+    'alert("loaded!");' \
+    '</script>' \
+    '<h1>fastpace</h1>' 
+
+  return script
