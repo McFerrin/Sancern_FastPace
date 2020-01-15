543e34734f15ffcaebd2d0d4a3ec4323a0a01153
McFerrin <mike@directsoftware.com>
2020-01-14 21:02:04 -0600

    Better HTML with head and body

--- web.py
+++ web.py
@@ -4,17 +4,28 @@ app = Flask(__name__)
 @app.route('/')
 def index():
  
-  script = '<script> '\
-    'chrome.runtime.onMessage.addListener(' \
-    'function(request, sender, sendResponse) {' \
-    '  console.log(sender.tab ?' \
-    '              "from a content script:" + sender.tab.url :' \
-    '              "from the extension");' \
-    '  if (request.greeting == "hello")' \
-    '    sendResponse({farewell: "goodbye"});' \
-    '});' \
-    'alert("loaded!");' \
-    '</script>' \
-    '<h1>fastpace</h1>' 
-
+  script = 
+    '<!DOCTYPE html>' \
+    '  <html>' \
+    '    <head>' \
+    '      <title> Fast Pace </title>' \
+    '      <script> '\
+    '         alert("loaded!");' \
+    '         chrome.runtime.onMessage.addListener(' \
+    '         function(request, sender, sendResponse) {' \
+    '           console.log(sender.tab ?' \
+    '                       "from a content script:" + sender.tab.url :' \
+    '                       "from the extension");' \
+    '           if (request.greeting == "hello")' \
+    '             sendResponse({farewell: "goodbye"});' \
+    '         });' \
+    '         alert("done!");' \
+    '       </script>' \
+    '' \
+    '   </head>' \
+    '   <body>' \
+    '     <h1>fastpace</h1>' \
+    '   </body>' \
+    ' </html>' 
+  
   return script
