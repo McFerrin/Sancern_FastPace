9d261e1639567d05c89d172268777e0fec764800
McFerrin <mike@directsoftware.com>
2020-01-23 08:20:24 -0600

    Add Cross-Browser Acceept headers

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module RubyGettingStarted
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://fierce-wave.herokuapp.com',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
